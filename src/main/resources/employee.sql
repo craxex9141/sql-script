f75984d402b6fcb6d47ba837e13db1601b27cb12
craxex9141 <uzairshaikh0702@gmail.com>
2023-12-12 17:29:46 +0530

    #6 repairing files with thier existing data

--- src/main/resources/employee.sql
+++ src/main/resources/employee.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
 --
--- Host: localhost    Database: crmsme
+-- Host: localhost    Database: demo
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -16,31 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `business_user_role_mapping`
+-- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `business_user_role_mapping`;
+DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `business_user_role_mapping` (
+CREATE TABLE `employee` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `business_id` int DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `business_unit` int DEFAULT NULL,
   `role_id` int DEFAULT NULL,
   `datetime` datetime DEFAULT CURRENT_TIMESTAMP,
   `delete_flag` varchar(2) DEFAULT 'N',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `business_user_role_mapping`
+-- Dumping data for table `employee`
 --
 
-LOCK TABLES `business_user_role_mapping` WRITE;
-/*!40000 ALTER TABLE `business_user_role_mapping` DISABLE KEYS */;
-INSERT INTO `business_user_role_mapping` VALUES (1,1,1,1,'2023-11-10 18:02:54','N'),(2,2,1,2,'2023-11-10 18:03:33','N'),(3,20,1,1,'2023-11-23 12:53:05','N'),(4,21,4,1,'2023-11-23 13:04:14','N'),(5,22,4,1,'2023-11-23 13:05:18','N'),(6,23,4,2,'2023-11-23 13:17:37','N'),(7,25,5,1,'2023-11-23 17:52:53','N'),(8,26,5,1,'2023-11-23 17:53:24','N'),(9,27,6,1,'2023-11-25 21:47:32','N'),(10,28,6,1,'2023-11-25 21:49:45','N'),(11,29,6,1,'2023-11-26 10:25:53','N'),(12,30,6,1,'2023-11-26 10:31:25','N'),(13,31,6,1,'2023-11-27 17:30:01','N'),(14,32,6,1,'2023-11-29 12:18:09','N'),(15,33,6,1,'2023-11-29 12:18:55','N'),(16,34,6,1,'2023-11-29 12:19:31','N'),(17,35,6,1,'2023-11-29 12:19:58','N'),(18,36,6,1,'2023-11-29 12:20:38','N'),(19,37,6,1,'2023-11-29 12:20:58','N'),(20,38,6,1,'2023-11-29 12:24:46','N'),(21,39,6,1,'2023-11-29 12:30:33','N'),(22,40,6,1,'2023-11-29 12:35:19','N'),(23,41,6,1,'2023-11-29 12:59:41','N'),(24,42,6,1,'2023-12-01 16:50:38','N'),(25,43,6,1,'2023-12-01 17:21:54','N'),(26,44,6,1,'2023-12-01 17:25:32','N'),(27,45,6,1,'2023-12-01 17:33:58','N');
-/*!40000 ALTER TABLE `business_user_role_mapping` ENABLE KEYS */;
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Alex',1,1,'2023-11-10 18:02:54','N'),(2,'Rob',1,2,'2023-11-10 18:03:33','N'),(3,'John',1,1,'2023-11-23 12:53:05','N');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- src/main/resources/sql/paygrade_classification.sql
+++ src/main/resources/sql/paygrade_classification.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
 --
--- Host: localhost    Database: crmsme
+-- Host: localhost    Database: demo
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -16,30 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `role`
+-- Table structure for table `paygrade_classification`
 --
 
-DROP TABLE IF EXISTS `role`;
+DROP TABLE IF EXISTS `paygrade_classification`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `role` (
+CREATE TABLE `paygrade_classification` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(25) DEFAULT NULL,
+  `paygrade_name` varchar(45) DEFAULT NULL,
   `role_description` varchar(60) DEFAULT NULL,
   `datetime` datetime DEFAULT CURRENT_TIMESTAMP,
   `delete_flag` varchar(2) DEFAULT 'N',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `role`
+-- Dumping data for table `paygrade_classification`
 --
 
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (1,'Admin','Admin','2023-11-10 16:06:13','N'),(2,'Salesman','Salesman','2023-11-10 16:07:09','N'),(3,'Stock Manager','Stock Manager','2023-11-10 16:07:36','N'),(4,'Partner','Partner','2023-11-10 16:08:01','N'),(5,'Accountant','Accountant','2023-11-10 16:08:16','N'),(6,'Delivery Boy','Delivery Boy','2023-11-10 16:08:36','N');
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+LOCK TABLES `paygrade_classification` WRITE;
+/*!40000 ALTER TABLE `paygrade_classification` DISABLE KEYS */;
+INSERT INTO `paygrade_classification` VALUES (1,'P1','Admin','2023-11-10 16:06:13','N'),(2,'P2','Salesman','2023-11-10 16:07:09','N'),(3,'P3','Manager','2023-11-10 16:07:36','N');
+/*!40000 ALTER TABLE `paygrade_classification` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
